245ee46282a61a0a2ee44d941d0ac8d79370837a
Cristy <cristy@no_spam.com>
2019-10-25 19:02:48 +0300

    Prepare openCV library for use

--- kernel.cu
+++ kernel.cu
@@ -1,7 +1,13 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv)
 {
+	cv::Mat image = cv::imread("image.jpg");
+	cv::namedWindow("image", cv::WINDOW_NORMAL);
+	cv::imshow("image", image);
+	cv::waitKey(0);
+
 	return 0;
 }
