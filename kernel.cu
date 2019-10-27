dd515c494cc18be85fd2635716047ecf5f66983d
unknown <comansergiu@ymail.com>
2019-10-27 11:26:46 +0200

    First commit Project does not work in Debug mode

--- kernel.cu
+++ kernel.cu
@@ -1,13 +1,24 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <opencv2/opencv.hpp>
-
+#include <exception>
 int main(int argc, char **argv)
 {
-	cv::Mat image = cv::imread("image.jpg");
-	cv::namedWindow("image", cv::WINDOW_NORMAL);
-	cv::imshow("image", image);
-	cv::waitKey(0);
-
+	try
+	{
+		cv::Mat image = cv::imread("image.jpg");
+		if (image.data == NULL)
+		{
+			std::cout << "NULL image\n";
+			return 1;
+		}
+		cv::namedWindow("image", cv::WINDOW_NORMAL);
+		cv::imshow("image", image);
+		cv::waitKey(0);
+	}
+	catch (std::exception exc)
+	{
+		std::cout << exc.what();
+	}
 	return 0;
 }
